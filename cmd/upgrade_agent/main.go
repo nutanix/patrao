a6650a44c0894200ed233ef428878f76a2b46aa8
Dmitry Gritsay <d.gritsay@quantumobile.com>
2019-04-19 18:30:52 +0300

    resolve compilation issue

--- cmd/upgrade_agent/main.go
+++ cmd/upgrade_agent/main.go
@@ -3,7 +3,7 @@ package main //import github.com/nutanix/patrao/
 import (
 	"os"
 
-	core "github.com/nutanix/patrao/internal/app/upgrade_agent/core"
+	core "github.com/nutanix/patrao/internal/app/upgrade_agent"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
--- internal/app/upgrade_agent/dockerManager.go
+++ internal/app/upgrade_agent/dockerManager.go
@@ -1,4 +1,4 @@
-package DockerManager
+package core
 
 func stub() {
 
